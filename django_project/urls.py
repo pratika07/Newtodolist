4e979562fe9742c4f4223050ab6ae55fcf93c22c
eshita-jain <80577092+eshita-jain@users.noreply.github.com>
2021-06-28 03:24:23 +0530

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,7 +4,7 @@ from users import views as user_views
 from django.urls import path, include
 from django.conf import settings
 
-# serving media files in DEBUG=FALSE mode, not appropriate for production
+
 from django.views.static import serve
 from django.conf.urls import url
 
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', user_views.logout_view, name='logout'), # using a custom logout function
-    #path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
+
     path('', include('blog.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), # serving media files in DEBUG=FALSE mode
 ]
