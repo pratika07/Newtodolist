b72ea86c09d77684f6cdbf642248953945b5ad0f
pratika07 <86431920+pratika07@users.noreply.github.com>
2021-06-28 16:43:52 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -4,28 +4,28 @@ from .models import Post
 from django.core.exceptions import ValidationError
 
 
-# creating a form for custom validation
+
 class CreateViewForm(forms.ModelForm):
 
     title = forms.CharField(required=False) # defining the field as not required
     image = forms.ImageField(required=False)
     audio = forms.FileField(required=False)
 
-    # Meta class is there for defining and altering various metadata features
+   
     class Meta:
         model = Post
         fields = ['title','content','date_posted','image','audio']
 
-    def clean(self): # validation function
+    def clean(self): 
         cleaned_data = super(CreateViewForm, self).clean()
 
-        input_field_1 = cleaned_data.get('title') # getting input from the title field
-        input_field_2 = cleaned_data.get('content') # getting input from the content field
+        input_field_1 = cleaned_data.get('title') 
+        input_field_2 = cleaned_data.get('content') 
 
         if input_field_1 is not None and input_field_2 is not None:
             if len(input_field_2)<3:
-                # self.add_error(None, ValidationError('The title should be different')) # non-field error
-                self.add_error('content', ValidationError('The message is too short')) # title field error
+               
+                self.add_error('content', ValidationError('The message is too short'))
 
 
 
