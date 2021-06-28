d16fb5905152e7c51734bb843f27c1d0eed2a1a4
pratika07 <86431920+pratika07@users.noreply.github.com>
2021-06-28 16:41:35 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -5,18 +5,17 @@ from django.urls import reverse
 from PIL import Image
 
 
-# defining a database table for posts
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now) # recording the timestamp when a new entry is created
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # ForeignKey defines a "many-to-one relationship"
+    date_posted = models.DateTimeField(default=timezone.now) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
     
 
     def __str__(self):
         return self.title
 
-    # defining a URL for a new entry
+   
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
 
